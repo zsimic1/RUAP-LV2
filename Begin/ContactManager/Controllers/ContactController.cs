3c19a9b1b624aacdc3ae1786cd7bb0381db04940
zsimic1 <zvonimir_simic@hotmail.com>
2020-12-07 18:10:46 +0100

    Prvi zadatak

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactManager.Services;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+        public Contact[] Get()
+            {
+                    return contactRepository.GetAllContacts();
+            }
+    }   
+
+
+}
