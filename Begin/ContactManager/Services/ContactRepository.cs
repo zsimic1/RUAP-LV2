f20a700430a01def558475888b186fd08f041081
zsimic1 <zvonimir_simic@hotmail.com>
2020-12-07 18:37:45 +0100

    Drugi zadatak

--- Begin/ContactManager/Services/ContactRepository.cs
+++ Begin/ContactManager/Services/ContactRepository.cs
@@ -8,21 +8,72 @@ namespace ContactManager.Services
 {
     public class ContactRepository
     {
+        private const string CacheKey = "ContactStore";
         public Contact[] GetAllContacts()
         {
-            return new Contact[]
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
             {
-            new Contact
-            {
-                Id = 1,
-                Name = "Glenn Block"
-            },
-            new Contact
-            {
-                Id = 2,
-                Name = "Dan Roth"
+                return (Contact[])ctx.Cache[CacheKey];
             }
-            };
+
+            return new Contact[]
+                {
+            new Contact
+            {
+                Id = 0,
+                Name = "Placeholder"
+            }
+                };
+        }
+        public ContactRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]
+                    {
+                new Contact
+                {
+                    Id = 1, Name = "Glenn Block"
+                },
+                new Contact
+                {
+                    Id = 2, Name = "Dan Roth"
+                }
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
         }
     }
 }
